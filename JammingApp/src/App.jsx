07d55747d061653f8b0b6b101099da766d347ed9
Levent <leventbilal.16@outlook.com>
2026-04-06 22:28:13 +0200

    spotify search API in implementation. client ID and client Secret are not valid

--- JammingApp/src/App.jsx
+++ JammingApp/src/App.jsx
@@ -1,8 +1,11 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 
 import Header from './components/Header/Header.jsx';
-import BodyContainer from './components/BodyContainer/BodyContainer.jsx'
+import BodyContainer from './components/BodyContainer/BodyContainer.jsx';
+
+const CLIENT_ID = '8352906fdc125e99870b97d1519dce6f';
+const CLIENT_SECRET = '1a896e0ec5784a4783bd827179e7fd5b';
 
 function App() {
 
@@ -10,6 +13,54 @@ function App() {
   const [trackListText, setTrackListText] = useState('');
   const [searchResults, setSearchResults] = useState([]);
   const [selectedSongs, setSelectedSongs] = useState([]);
+  const [accessToken, setAccessToken] = useState('');
+
+  useEffect(() => {
+    //  API Access Token
+    const authParameters = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      body: 'grant_type=client_credentials&client_id=' + CLIENT_ID + '&client_secret=' + CLIENT_SECRET,
+      cache: 'no-cache'
+    }
+    fetch('https://accounts.spotify.com/api/token', authParameters)
+    .then(result => {
+      if(result.ok){
+        return result.json()
+      }
+      throw new Error('Request Failed!');
+    }, networkError => {
+      console.log(networkError.message);
+    })
+    .then(data => {
+      //  console.log(data.access_token);
+      setAccessToken(data.access_token);
+    })
+  }, []);
+
+  async function searchSpotify(){
+    console.log('Search for ' + searchText);
+
+    //  get request using search to get the Artist ID
+
+    let artistParameters = {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + accessToken
+      }
+    }
+
+    let artistID = await fetch('https://api.spotify.com/v1/search?q=' + searchText + '&type=artist', artistParameters)
+    .then(response => response.json())
+    .then(data => console.log(data));
+    // get request with Artist ID grab all the albums from the artist
+
+    // Display albums to user
+
+  };
 
   const searchEntryHandler = e => {
     setSearchText(e.target.value);
@@ -38,6 +89,7 @@ function App() {
         )
       }
       setSearchResults(songList);
+      searchSpotify();
     } else{
       setSearchResults([]);
     }
